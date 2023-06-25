a117c0e763c76e9009ae67b77e6931876c2724aa
SimonFrank <frank.simon20@gmail.com>
2023-06-25 17:12:42 -0400

    Simon changes -testing auto deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. hayden change 2
+          Edit <code>src/App.js</code> and save to reload. simon change
         </p>
         <a
           className="App-link"
