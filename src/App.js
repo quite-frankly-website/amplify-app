a4800f01ad10fb95e5a733cb0b94d9fbca2889cc
SimonFrank <frank.simon20@gmail.com>
2023-06-25 21:44:31 -0400

    Testing dev

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. simon change
+          Edit <code>src/App.js</code> and save to reload. simon change DEV
         </p>
         <a
           className="App-link"
